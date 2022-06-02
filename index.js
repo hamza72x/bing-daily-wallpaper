fc032b2013da7aac1a295b6887b4ec98c5e0e1e7
hamza <hamza72x@gmail.com>
2022-06-02 09:58:30 +0600

    Added 4K support

--- index.js
+++ index.js
@@ -8,46 +8,47 @@ const DailyWallpaper = require("daily-wallpaper");
  * (and with an optionnal error as first parameter).
  */
 async function _getBingWallpaper() {
-  try {
-    const res = await fetch(
-      "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-    );
-    const {
-      images: [wallpaperSource],
-    } = await res.json();
-    return wallpaperSource;
-  } catch (err) {
-    console.error("Could not fetch wallpaper source from Bing");
-    throw err;
-  }
+    try {
+        const res = await fetch(
+            "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+        );
+        const {
+            images: [wallpaperSource],
+        } = await res.json();
+        return wallpaperSource;
+    } catch (err) {
+        console.error("Could not fetch wallpaper source from Bing");
+        throw err;
+    }
 }
 
 /**
  * Fetches and set the current bing wallpaper as wallpaper
  */
 module.exports = async function (directory) {
-  const dailyWallpaper = new DailyWallpaper();
+    const dailyWallpaper = new DailyWallpaper();
 
-  dailyWallpaper.getWallpaperSource = async function (done) {
-    try {
-      const source = await _getBingWallpaper();
-      done(null, {
-        url: "http://www.bing.com" + source.url,
-        extension: "jpg",
-      });
-    } catch (err) {
-      done(err);
-    }
-  };
+    dailyWallpaper.getWallpaperSource = async function (done) {
+        try {
+            const source = await _getBingWallpaper();
+            const url = source.url.replaceAll("_1920x1080", "_UHD");
+            done(null, {
+                url: "http://www.bing.com" + url,
+                extension: "jpg",
+            });
+        } catch (err) {
+            done(err);
+        }
+    };
 
-  dailyWallpaper.setDirectory(directory);
+    dailyWallpaper.setDirectory(directory);
 
-  return new Promise((resolve, reject) => {
-    dailyWallpaper.setDailyWallpaper(function (err) {
-      if (err) {
-        reject(err);
-      }
-      resolve();
+    return new Promise((resolve, reject) => {
+        dailyWallpaper.setDailyWallpaper(function (err) {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
     });
-  });
 };
